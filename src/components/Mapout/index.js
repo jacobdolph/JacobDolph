242201f5d87ca72dfd919a8b666bddc8639cc018
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-14 18:34:42 -0600

    back to sqaure one

--- src/components/Mapout/index.js
+++ src/components/Mapout/index.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-
+import { Col } from '../Grid'
 import portfolio from '../../portfolio.json'
 
 
-export default {
+function Mapout() {
 
-    projectsMapOne: function () {
+    return (
         portfolio.map(portfolio => {
-            if (portfolio.id < 4) {
-                return portfolio;
-            }
+            return <Col size='s12 m4' key={portfolio.id} className="z-depth-2" ><a href={portfolio.projectUrl}><img src={portfolio.image} alt={portfolio.name} style={{ width: '7rem' }} /></a><h6>{portfolio.name}</h6><a href={portfolio.repoUrl}><h6>Repo</h6></a></Col>
         })
-    },
-
-    projectsMapTwo: function () {
-        portfolio.map(portfolio => {
-            if (portfolio.id > 3) {
-                return portfolio;
-            }
-        })
-    }
-
-
-
-
-
+    )
 }
 
+export default Mapout;
