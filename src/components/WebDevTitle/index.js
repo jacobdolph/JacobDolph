b23d9b46926a5b4931753266f7f976597f414a5e
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-16 11:30:12 -0600

    created a component for the webdevtitle

---
+++ src/components/WebDevTitle/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function WebDevTitle() {
+    return (
+
+        <div>
+            <h1 className='flow-text' style={{ margin: '3rem 0 ', fontSize: "2.5rem" }}>Web Developer</h1>
+        </div>
+
+    )
+}
+
+export default WebDevTitle;
