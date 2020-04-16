b37cde5dc48a1d42fc95f1b1b9d1e6e77aa3ff67
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-15 20:09:45 -0600

    lined up tech stack for mobile and desktop view

--- src/components/Tech/index.js
+++ src/components/Tech/index.js
@@ -9,34 +9,34 @@ function Tech() {
                 <div className="tech center">
                     <div className="icons col s6 m3 ">
                         <i className="fab fa-react blue-text text-lighten-3" style={{ fontSize: '5rem' }}></i><br />
-                        <p style={{ fontSize: "2rem;", color: "#555555;" }}>React</p>
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>React</p>
                     </div>
                     <div className="icons col s6 m3 ">
                         <i className="fab fa-html5 red-text text-lighten-3" style={{ fontSize: '5rem' }}></i><br />
-                        <p style={{ fontSize: "2rem;", color: "#555555;" }}>HTML5</p>
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>HTML5</p>
                     </div>
                     <div className="icons col s6 m3">
                         <i className="fab fa-css3-alt blue-text text-lighten-3" style={{ fontSize: '5rem' }}></i>
-                        <p style={{ fontSize: "2rem;", color: "#555555;" }}>CSS</p>
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>CSS</p>
                     </div>
                     <div className="icons col s6 m3">
                         <i className="fab fa-js yellow-text text-lighten-3" style={{ fontSize: '5rem' }}></i>
-                        <p style={{ fontSize: "2rem;", color: "#555555;" }}>JavaScript</p>
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>JavaScript</p>
                     </div>
                 </div>
                 <div className="tech center">
                     <div className="icons col s6 m3">
                         <i className="fab fa-node-js green-text text-lighten-3" style={{ fontSize: '5rem' }}></i>
-                        <p style={{ fontSize: "2rem;", color: "#555555;" }}>Node</p>
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>Node</p>
                     </div>
                     <div className="icons col s6 m3">
-                        <img src={MongoDb} style={{ height: "4.5rem" }} alt="image of mysql-workbench" srcset="" />
-                        <p style={{ fontSize: "1rem", color: "#555555" }}>MongoDb</p>
+                        <img src={MongoDb} style={{ height: "4.5rem" }} alt="mysql-workbench" />
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>MongoDb</p>
                     </div>
                     <div className="icons col s6 m3">
-                        <img src={MySql} style={{ height: '4.5rem' }} alt="image of mysql-workbench"
-                            srcset="" />
-                        <p style={{ fontSize: "2rem;", color: "#555555;" }}>MySql</p>
+                        <img src={MySql} style={{ height: '4.5rem' }} alt="mysql-workbench"
+                        />
+                        <p style={{ fontSize: "2rem", color: "#555555" }}>MySql</p>
                     </div>
                 </div>
 
