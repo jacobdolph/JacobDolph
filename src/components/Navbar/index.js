fefd4f58466d65c57246993d0f4b84de99df3400
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-15 22:22:07 -0600

    changed the color of the navbar to blue grey

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,14 +13,14 @@ export class Navbar extends Component {
     render() {
         return (
             <div className='navbar-fixed'>
-                <nav className='z-depth-0' style={{ backgroundColor: 'rgba(255, 255, 255, 1)' }}>
+                <nav className='z-depth-0' style={{ backgroundColor: '#37474f' }}>
                     <div className='nav-wrapper'>
-                        <a href="#" data-target="mobile-demo" className="sidenav-trigger left"><i className="material-icons" style={{ color: '#212121' }}>menu</i></a>
-                        <ul id='nav-mobile' className="left hide-on-med-and-down" style={{ margin: '1rem', color: '#212121' }} >
-                            <li><Link to='/' className='black-text' >Home</Link></li>
-                            <li><Link to='/' className='black-text'>Portfolio</Link></li>
-                            <li><Link to='/' className='black-text'>About</Link></li>
-                            <li><Link to='/' className='black-text'>Connect</Link></li>
+                        <a href="#" data-target="mobile-demo" className="sidenav-trigger left"><i className="material-icons" style={{ color: 'white' }}>menu</i></a>
+                        <ul id='nav-mobile' className="left hide-on-med-and-down" style={{ margin: '0rem' }} >
+                            <li><Link to='/' className='white-text' >Home</Link></li>
+                            <li><Link to='/' className='white-text'>Portfolio</Link></li>
+                            <li><Link to='/' className='white-text'>About</Link></li>
+                            <li><Link to='/' className='white-text'>Connect</Link></li>
                         </ul>
                     </div>
                 </nav>
