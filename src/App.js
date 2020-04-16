a45e4398f5a6056b97bdd040ebfc8778eecaeebc
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-15 18:48:21 -0600

    removed unused pages, going with a one page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-
 import './index.css'
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
-
-import Connect from './pages/Connect';
 import About from './pages/About';
 
 
@@ -13,14 +9,9 @@ function App() {
   return (
     <Router>
       <div>
-
         <Switch>
           <Route exact path='/' component={Home} />
-
-          <Route exact path='/about' component={About} />
-          <Route exact path='/connect' component={Connect} />
         </Switch>
-
       </div>
     </Router>
   )
