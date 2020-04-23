c18adb4dafadd26ef8b30e9290af95ee0b2af7b3
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-23 17:14:38 -0600

    Updates

---
+++ precache-manifest.fe923ed0ab1020fd1751b88964fbba74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5848085da27a49ab35c0a87429639216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c23d837d4cbfa75c53b7",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "e9ad6f11d06b145778d6",
+    "url": "/static/css/main.cba62fda.chunk.css"
+  },
+  {
+    "revision": "c23d837d4cbfa75c53b7",
+    "url": "/static/js/2.ef0b5999.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/static/js/2.ef0b5999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ad6f11d06b145778d6",
+    "url": "/static/js/main.de907ca0.chunk.js"
+  },
+  {
+    "revision": "a0ac1140309c735bc60b",
+    "url": "/static/js/runtime-main.6be4d7d3.js"
+  },
+  {
+    "revision": "cb3fd0f8501b3b6bf720c2a188a7badd",
+    "url": "/static/media/mysql-workbench-icon.cb3fd0f8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactPortfolio/precache-manifest.820d154720be20ce647750caa913c818.js"
+  "/precache-manifest.fe923ed0ab1020fd1751b88964fbba74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
